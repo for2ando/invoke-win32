8ecfeb504f3628860e5afc823ba54148dc726e68
Atmosfere Forzando <forzandoe@gmail.com>
2014-08-14 17:07:08 +0900

    Dynamic reconstruction

--- junk/MySession.ps1
+++
@@ -1,2 +0,0 @@
-start-process powershell -noexit,-command,"& 'C:\Documents and Settings\All Users\スタート メニュー\0 winap\p Windows PowerShell\Modify-PowerShellWindow.ps1'"
-start-process powershell
--- junk/Wide PowerShell.ps1
+++
@@ -1,9 +0,0 @@
-#
-$winWidth = 132
-$pswin = $(get-host).ui.rawui
-$bufsiz = $pswin.buffersize
-$bufsiz.width = $winWidth
-$pswin.buffersize = $bufsiz
-$winsiz = $pswin.windowsize
-$winsiz.width = $winWidth
-$pswin.windowsize = $winsiz
